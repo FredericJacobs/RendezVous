8e2cf408313144925693b18857a98301737bcd89
Christine Corbett Moran <christine.corbett@gmail.com>
2013-04-20 15:25:30 +0200

    basic setup of viewbased iOS app with placeholder earth/moon image

---
+++ sat/sat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sat
+//
+//  Created by Christine Corbett Moran on 4/20/13.
+//  Copyright (c) 2013 Space Apps Challenge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
